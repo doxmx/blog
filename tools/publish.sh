f99540cd82f47c3fda99da8df95d4e18707670d4
Tony Garcia <tony.garcia@rackspace.com>
2018-11-17 00:25:34 -0600

    Updating gh-pages submodule

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,7 +3,11 @@
 hugo
 cd public
 git add .
-msg="Rebuild site $(date +%F %T)"
-git commit -m "$msg" .
+msg_sub="Rebuild site $(date +%F %T)"
+git commit -m "${msg_sub}" .
 git push origin master
 cd ..
+git add .
+msg="Updating gh-pages submodule"
+git commit -m "${msg}"
+git push origin master
